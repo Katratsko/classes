5da6c745d2900b34b8fa3b4d8d627d7474e72b8e
Ekaterina <ekaterinaratkova@gmail>
2022-02-01 22:30:38 +0300

    init

---
+++ lab1/main.py
@@ -0,0 +1,3 @@
+a=input("введите строку")
+b=len(a)
+print(b)
---
+++ lab1/возраст.py
@@ -0,0 +1,14 @@
+money = int(input("сколько денег на карточке"))
+cost = int(input(" введите расходы"))
+for i in range(money):
+    for j in range(cost):
+        if i<=j:
+            c=money-cost
+            print(c)
+            print(j)
+            if money<cost:
+                print("stop")
+            else:
+                print("продолжаем покупки")
+
+
